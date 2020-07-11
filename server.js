c9228d9defdfcf81f5bb53c0b89839730387c7a9
Emmanuel <pacesetter4lyf@gmail.com>
2020-07-11 21:02:26 +0100

    my first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const knex = require('knex')
+//const bcrypt = require('')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'me',
+    password : 'password',
+    database : 'smart-brain'
+  }
+});
+
+db.select('*').from('users').then( data => {
+	//console.log(data);
+});
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+	res.send(database.users);
+});
+app.post("/signin", (req, res) => {signin.handleSignin(req, res, db)});
+
+app.post("/register", (req, res) => {register.handleRegister(req, res, db)})
+
+app.get("/profile/:id", (req, res) => {profile.handleProfileGet(req, res, db)});
+
+app.put("/image", (req, res) => {image.imgCount(req, res, db)});
+
+
+app.listen(3001, () => {
+	console.log("I am listening")
+})
+

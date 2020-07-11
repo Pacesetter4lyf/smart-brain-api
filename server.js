913179cc611c120d87e3ab6a759ae63717b7d9b5
Emmanuel <pacesetter4lyf@gmail.com>
2020-07-11 22:27:15 +0100

    changing db in the root route

--- server.js
+++ server.js
@@ -26,8 +26,11 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors())
 
+// app.get("/", (req, res) => {
+// 	res.send(database.users);
+// });
 app.get("/", (req, res) => {
-	res.send(database.users);
+	res.send("it is working");
 });
 app.post("/signin", (req, res) => {signin.handleSignin(req, res, db)});
 
